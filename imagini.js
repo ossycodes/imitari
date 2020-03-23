566cfb8c2533c50191528fe624e3f0be7503e5d9
osaigbovoemmanuel1 <osaigbovoemmanuel1@gmail.com>
2020-03-23 18:02:32 +0100

    added more image manipulating options: flip, flop, sharpen, blur

--- imagini.js
+++ imagini.js
@@ -26,12 +26,15 @@ function download_image(req, res) {
             return res.status(404).end();
         }
 
-        
+
         let image = sharp(req.localpath);
         let width = +req.query.width;
         let height = +req.query.height;
+        let blur = +req.query.blur;
+        let sharpen = +req.query.sharpen;
         let greyscale = ["y", "yes", "1", "on"].includes(req.query.greyscale);
-        
+        let flip = ["y", "yes", "1", "on"].includes(req.query.flip);
+        let flop = ["y", "yes", "1", "on"].includes(req.query.flop);
 
         if (width > 0 && height > 0) {
             /**
@@ -49,6 +52,23 @@ function download_image(req, res) {
             image.resize(width || null, height || null);
         }
 
+
+        if (flip) {
+            image.flip();
+        }
+
+        if (flop) {
+            image.flop();
+        }
+
+        if (blur > 0) {
+            image.blur(blur);
+        }
+
+        if (sharpen > 0) {
+            image.sharpen(sharpen);
+        }
+        
         if (greyscale) {
             image.greyscale();
         }
